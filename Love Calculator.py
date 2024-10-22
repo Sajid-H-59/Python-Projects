d5c14cee219fb3a1d3138764e399c8b7156b5244
Sajid-H-59 <sajidhossain2606@gmail.com>
2024-10-22 22:43:29 +0600

    Delete Love Calculator.py

--- Love Calculator.py
+++
@@ -1,6 +0,0 @@
-import random
-print("Welcome to Love Calculator!")
-name1 = str(input("What is your name? \n"))
-name2 = str(input("What's your crush's name? \n"))
-
-print(f"Your love score is {random.randint(1,100)}")
